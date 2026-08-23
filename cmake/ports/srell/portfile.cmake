3141bf5c8d5ef9415d3f1f2749a25aceb8aa6e8e
areqpl <areqpl@users.noreply.github.com>
2026-08-23 22:35:01 +0200

    fix(vcpkg): add Cloudflare Pages download mirror and update SHA512 hash for srell port

--- cmake/ports/srell/portfile.cmake
+++ cmake/ports/srell/portfile.cmake
@@ -2,9 +2,10 @@ string(REPLACE "." "_" VERSION ${VERSION})
 
 vcpkg_download_distfile(
     ARCHIVE
-    URLS "https://www.akenotsuki.com/misc/srell/releases/srell${VERSION}.zip"
+    URLS "https://akenotsuki.pages.dev/misc/srell/releases/srell${VERSION}.zip"
+         "https://www.akenotsuki.com/misc/srell/releases/srell${VERSION}.zip"
     FILENAME "srell${VERSION}.zip"
-    SHA512 7c834cdc3b1cc63b3961601429e3d788f182469411aac5377801407550de0313d8dd1422355f1a0d78c77c2b141f75068312140484f4d8f87c3b2e76b4b1c754
+    SHA512 056adc3a22bd44ba559dc5ab5c956d2e24a5e490d7c35324f2d566604dd32e9ebc9954eb879726bbb8bf333a28b27db4ebc124dbe4093914307dd9128c784164
 )
 
 vcpkg_extract_source_archive(
